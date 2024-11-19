71ed91066cd547d4f0de284fc5e86c7f759b062a
Kingtex <t.tanne29@gmail.com>
2024-11-19 15:22:52 +0100

    add post

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,24 +1,34 @@
 from fastapi import FastAPI
 from databaseConnect import databaseConnect
+from psycopg2.extras import RealDictCursor
 import uvicorn
 app = FastAPI()
 
 @app.get("/maregraphe")
 async def getMaregraphe():
-    cur = databaseConnect()
+    db = databaseConnect()
+    cur = db.cursor(cursor_factory=RealDictCursor)
     cur.execute("SELECT * FROM obsmar.maregraphe")
     return cur.fetchall()
 
 @app.get("/metadonnee/{id}")
 async def root(id: int):
-    cur = databaseConnect()
+    db = databaseConnect()
+    cur = db.cursor(cursor_factory=RealDictCursor)
     cur.execute(f"SELECT * FROM obsmar.maregraphe_meta WHERE id_maregraphe = {id}")
     return cur.fetchall()
 
+@app.post("/addMeta/{id}&{meta}&{data}")
+async def addMeta(id: int, meta: str, data: str):
+    db = databaseConnect()
+    cur = db.cursor(cursor_factory=RealDictCursor)
+    cur.execute(f"INSERT INTO obsmar.maregraphe_meta (id_maregraphe, id_meta, donnee) VALUES ({id}, '{meta}', '{data}')")
+    db.commit()
+    return cur.lastrowid
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/hello/{name}&{test}")
+async def say_hello(name: str, test: str):
+    return {"message": f"Hello {name} {test}"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
