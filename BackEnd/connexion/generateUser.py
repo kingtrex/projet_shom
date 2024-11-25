0da1862cc23b692c59ddaf1e6513351ccca45bcb
Kingtex <t.tanne29@gmail.com>
2024-11-25 10:13:40 +0100

    Token with file

---
+++ BackEnd/connexion/generateUser.py
@@ -0,0 +1,26 @@
+import configparser
+
+config = configparser.ConfigParser()
+
+config["USERS"] = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+with open("exemple.ini", "w") as file:
+    config.write(file)
+
+    file.close()
+
