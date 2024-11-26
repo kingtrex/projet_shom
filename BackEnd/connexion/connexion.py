a4c7218e4e40277e7edc681ac139233d57f051b7
Kingtex <t.tanne29@gmail.com>
2024-11-26 10:44:44 +0100

    Token v2

--- BackEnd/connexion/connexion.py
+++ BackEnd/connexion/connexion.py
@@ -1,19 +1,36 @@
+import configparser
+from datetime import timedelta, timezone, datetime
+
+import jwt
+
 from typing import Annotated
 
 from fastapi import Depends, APIRouter, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from starlette import status
+from jwt.exceptions import InvalidTokenError
 
-import configparser
+from passlib.context import CryptContext
+
 
 import os
 
-
 import ast
+
+SECRET_KEY = "292eceaf7266e55b10d28c87659f6bc16f8366d62cf687bf929d92c1e31c4a4c"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="connexion/token")
 router = APIRouter()
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
 
 class User(BaseModel):
     username: str
@@ -24,40 +41,64 @@ class User(BaseModel):
 class UserInDB(User):
     hashed_password: str
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
-    )
 
-def fake_hash_password(password: str):
-    return "fakehashed" + password
+def verify_password(plain_password, hashed_password):
+    print(f"password: {plain_password}")
+    return pwd_context.verify(plain_password, hashed_password)
 
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db, username: str):
+
+    if db.has_option("USERS", username):
+        user_dict = ast.literal_eval(db["USERS"][username])
+        return UserInDB(**user_dict)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    fichierConf = configparser.ConfigParser()
+    fichierConf.sections()
+    fichierConf.read("connexion/exemple.ini")
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fichierConf, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
 
@@ -69,22 +110,31 @@ async def get_current_active_user(
     return current_user
 
 @router.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+
     fichierConf = configparser.ConfigParser()
     fichierConf.sections()
     fichierConf.read("connexion/exemple.ini")
-
-    #vérifier si le login est dans le fichier
     if not fichierConf.has_option("USERS", form_data.username):
         print("mauvais login")
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user_dict = ast.literal_eval(fichierConf.get("USERS", form_data.username))
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    #Vérifier si le password est bon
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    return {"access_token": user.username, "token_type": "bearer"}
+
+    user = authenticate_user(fichierConf, form_data.username, form_data.password)
+    if not user:
+        print("mauvais mdp")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
 @router.get("/items/")
 async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"token": token}
