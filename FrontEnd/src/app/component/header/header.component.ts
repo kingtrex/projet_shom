47a933a0479fde8b71b82f064a44c6e4ef91a571
Quentinlm29 <lemenahezequentin@gmail.com>
2024-11-28 09:38:55 +0100

    ajout bouton deconnexion fonctionnel

--- FrontEnd/src/app/component/header/header.component.ts
+++ FrontEnd/src/app/component/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { AuthService } from '../../services/auth_service/auth.service'
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,10 @@ import { Component} from '@angular/core';
 })
 
 export class HeaderComponent{
+
+  constructor(private authservice: AuthService){}
+
+  public async deconnexion(){
+    this.authservice.logout();
+  }
 }
