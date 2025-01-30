859ee48169e41d007c4210e49068d85a2b391487
Kingtex <t.tanne29@gmail.com>
2025-01-30 16:36:28 +0100

    refactor: optimize database queries in meta API and remove redundant code

--- BackEnd/api/meta.py
+++ BackEnd/api/meta.py
@@ -1,5 +1,5 @@
 from psycopg2.extras import RealDictCursor
-from databaseConnect import database_connect, check_error
+from databaseConnect import database_connect, check_error, execute_query
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
 
@@ -8,85 +8,53 @@ router = APIRouter()
 
 @router.get("/getMetaId/")
 async def get_meta_id(token: Annotated[User, Depends(get_current_user)]):
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        cur.execute(f"SELECT * FROM obsmar.meta \
-                    ORDER BY id")
-        return cur.fetchall()
-    except Exception as e:
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
 
+    query = "SELECT * FROM obsmar.meta \
+            ORDER BY id"
+    result = await execute_query(query, True)
+    return result
 
 @router.get("/sort/{col}&{order}")
 async def sort_meta(col: str, order: bool, token: Annotated[User, Depends(get_current_user)]):
     allow_col = ["id", "ordre"]
     if col not in allow_col:
         raise HTTPException(status_code=500, detail="Nom de colonne invalide")
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        query = f"SELECT * FROM obsmar.meta ORDER BY {col} "
-        if order:
-            query += "DESC"
-        cur.execute(query)
-        return cur.fetchall()
-    except Exception as e:
-        print(e)
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = f"SELECT * FROM obsmar.meta ORDER BY {col} "
+    if order:
+        query += "DESC"
+    print(query)
+    result = await execute_query(query, True)
+    return result
 
 @router.post("/addMeta/{id}&{desc}&{ordre}")
 async def add_meta(id: str, desc: str, ordre: int,
                    token: Annotated[User, Depends(get_current_user)]):
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        cur.execute(f"INSERT INTO obsmar.meta VALUES (%s, %s, %s)", (id, desc, ordre))
-        db.commit()
-        return cur.lastrowid
-    except Exception as e:
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = "INSERT INTO obsmar.meta VALUES (%s, %s, %s)"
+    param = (id, desc, ordre)
+    result = await execute_query(query, False, param)
+    return result
 
 @router.put("/updateMeta/{id}&{desc}&{ordre}")
 async def update_meta(id: str, desc: str, ordre: int,
                       token: Annotated[User, Depends(get_current_user)]):
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        cur.execute(f"UPDATE obsmar.meta \
-                    SET description=%s, ordre=%s \
-                    WHERE id=%s", (desc, ordre, id))
-        db.commit()
-        return cur.lastrowid
-    except Exception as e:
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = "UPDATE obsmar.meta \
+            SET description=%s, ordre=%s \
+            WHERE id=%s"
+    param = (desc, ordre, id)
+    result = await execute_query(query, False, param)
+    return result
 
 @router.delete("/deleteMeta/{id}")
 async def delete_meta(id: str,
                       token: Annotated[User, Depends(get_current_user)]):
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        cur.execute(f"DELETE FROM obsmar.maregraphe_meta "
-                    f"WHERE id_meta=%s", (id,))
-        cur.execute(f"DELETE FROM obsmar.meta \
-                    WHERE id=%s", (id,))
-        db.commit()
-        return cur.lastrowid
-    except Exception as e:
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = "DELETE FROM obsmar.maregraphe_meta WHERE id_meta=%s"
+    param = (id,)
+    await execute_query(query, False, param)
+    query = "DELETE FROM obsmar.meta WHERE id=%s"
+    param = (id,)
+    result = await execute_query(query, False, param)
+    return result
