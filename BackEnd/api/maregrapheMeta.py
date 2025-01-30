181327a6af9cb9861c3ddf87c32c2cd6ba5d6f25
Kingtex <t.tanne29@gmail.com>
2025-01-30 16:19:49 +0100

    refactor: streamline database operations in maregrapheMeta API and update service calls

--- BackEnd/api/maregrapheMeta.py
+++ BackEnd/api/maregrapheMeta.py
@@ -1,4 +1,4 @@
-from databaseConnect import database_connect, check_error
+from databaseConnect import database_connect, check_error, execute_query
 from psycopg2.extras import RealDictCursor
 from typing import Annotated
 
@@ -13,118 +13,73 @@ router = APIRouter()
 @router.get("/getMeta/{id}")
 async def get_maregraphe_meta(id: int,
                token: Annotated[User, Depends(get_current_user)]):
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        cur.execute(f"SELECT * FROM obsmar.maregraphe_meta \
-                    WHERE id_maregraphe = %s \
-                    ORDER BY id_meta", (id,))
-        return cur.fetchall()
-    except Exception as e:
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = "SELECT * FROM obsmar.maregraphe_meta WHERE id_maregraphe=%s ORDER BY id_meta"
+    result = await execute_query(query, True, (id,))
+    return result
+
 
 @router.get("/sort/{id}&{col}&{order}")
 async def sort_maregraphe_meta(id: int, col: str, order: bool, token: Annotated[User, Depends(get_current_user)]):
     allowed_col = ["id_meta", "date_donnee"]
     if col not in allowed_col:
         raise HTTPException(status_code=500, detail="Nom de colonne invalide")
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        query = f"SELECT * FROM obsmar.maregraphe_meta WHERE id_maregraphe=%s ORDER BY {col} "
-        if order:
-            query += f"DESC"
-        data = cur.execute(query, (id,))
-        print(data)
-        return cur.fetchall()
-    except Exception as e:
-        print(e)
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = f"SELECT * FROM obsmar.maregraphe_meta WHERE id_maregraphe=%s ORDER BY {col} "
+    if order:
+        query += f"DESC"
+
+    result = await execute_query(query, True, (id,))
+    return result
 
 @router.post("/addMeta/{id}&{meta}&{data}")
 async def add_meta(id: int, meta: str, data: str,
                    token: Annotated[User, Depends(get_current_user)]):
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        cur.execute(f"INSERT INTO obsmar.maregraphe_meta \
-                    VALUES (%s, %s, %s, %s)", (id, meta, data, datetime.now()))
-        db.commit()
-        return cur.lastrowid
-    except Exception as e:
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = "INSERT INTO obsmar.maregraphe_meta \
+            VALUES (%s, %s, %s, %s)"
+    param = (id, meta, data, datetime.now())
+    result = await execute_query(query, False, param)
+    return result
 
 @router.put("/updateMeta/{id}&{meta}&{data}")
 async def update_meta(id: int, meta: str, data: str,
                       token: Annotated[User, Depends(get_current_user)]):
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        cur.execute(f"UPDATE obsmar.maregraphe_meta SET donnee = %s \
-                    WHERE id_maregraphe = %s AND id_meta = %s", (data, id, meta))
-        db.commit()
-        return cur.lastrowid
-    except Exception as e:
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = f"UPDATE obsmar.maregraphe_meta SET donnee = %s \
+            WHERE id_maregraphe = %s AND id_meta = %s"
+    param = (data, id, meta)
+    result = await execute_query(query, False, param)
+    return result
 
 @router.delete("/deleteAllMeta/{id}")
 async def delete_meta(id: int,
                      token: Annotated[User, Depends(get_current_user)]):
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        cur.execute(f"DELETE FROM obsmar.maregraphe_meta \
-                    WHERE id_maregraphe = %s", (id,))
-        db.commit()
-        return cur.lastrowid
-    except Exception as e:
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = "DELETE FROM obsmar.maregraphe_meta \
+            WHERE id_maregraphe = %s"
+    param = (id, )
+    result = await execute_query(query, False, param)
+    return result
 
 @router.delete("/deleteMeta/{id}&{meta}")
 async def delete_meta(id: int, meta: str,
                       token: Annotated[User, Depends(get_current_user)]):
-    try:
-        print(f"{id} - {meta}")
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        cur.execute(f"DELETE FROM obsmar.maregraphe_meta \
-                    WHERE id_maregraphe = %s AND id_meta = %s", (id, meta))
-        db.commit()
-        return cur.lastrowid
-    except Exception as e:
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = "DELETE FROM obsmar.maregraphe_meta \
+            WHERE id_maregraphe = %s AND id_meta = %s"
+    param = (id, meta)
+    result = await execute_query(query, False, param)
+    return result
 
 @router.get("/getMetaForm/{id}")
 async def get_meta_form(id: int,token: Annotated[User, Depends(get_current_user)]):
-    try:
-        db = database_connect()
-        cur = db.cursor(cursor_factory=RealDictCursor)
-        cur.execute(f"SELECT * FROM obsmar.meta me \
-                    WHERE NOT EXISTS(\
-                        SELECT * FROM obsmar.maregraphe_meta ma \
-                        WHERE me.id=ma.id_meta AND ma.id_maregraphe=%s\
-                    ) ORDER BY me.id", (id,))
-        return cur.fetchall()
-    except Exception as e:
-        check_error(e)
-    finally:
-        if 'db' in locals() and db:
-            db.close()
+
+    query = "SELECT * FROM obsmar.meta me \
+            WHERE NOT EXISTS(\
+                SELECT * FROM obsmar.maregraphe_meta ma \
+                WHERE me.id=ma.id_meta AND ma.id_maregraphe=%s\
+            ) ORDER BY me.id"
+    param = (id,)
+    result = await execute_query(query, True, param)
+    return result
